3306d87be563fbf9174cf18660d31f48941bfa24
= <=>
2020-11-14 17:40:39 +0200

    Add New Text

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -23,7 +23,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    
+                    Heloo Eslam
                 </div>
             </div>
         </div>
